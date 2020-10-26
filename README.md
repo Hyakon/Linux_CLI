67d8e8ef544242f2e85ba9cdd847767ad4f66e05
Hyakon <hyakon@pop-os.localdomain>
2020-10-26 19:23:29 +0100

    add autosuggest zsh

---
+++ README.md
@@ -0,0 +1,7 @@
+Usefull CLI
+
+To execute .sh file use :
+```shell
+chmod +x file_name.sh
+./file_name.sh
+```
---
+++ ZSH/install.sh
@@ -0,0 +1,10 @@
+sudo apt install zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+cp .zshrc ~/.zshrc
+
+source ~/.zshrc
