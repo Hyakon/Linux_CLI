1b1a7ec83b0ce149fc227d38387549c05aea1d70
Hyakon <hyakon@pop-os.localdomain>
2020-10-26 20:09:25 +0100

    mouse shorcut in progress

---
+++ mouse_shortcut/xbindkeys/README.md
@@ -0,0 +1,4 @@
+not finished : Do not run 
+
+More info :
+https://www.linuxuprising.com/2019/11/how-to-bind-mouse-buttons-to-keyboard.html
---
+++ mouse_shortcut/xbindkeys/install.sh
@@ -0,0 +1,9 @@
+# install xbindkeys
+sudo apt-get install xbindkeys-config
+
+# Create config file
+xbindkeys --defaults > ~/.xbindkeysrc
+
+
+xbindkeys-config
+
